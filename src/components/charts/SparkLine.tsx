7f71beea4dd525f99ba70b6d9995dd4853b030c1
Abdelrahman Khaled <abdelrahman@Abdelrahmans-iMac.local>
2022-05-30 19:13:32 +0200

    Folder structure done

---
+++ src/components/charts/SparkLine.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SparkLine: React.FC = () => {
+  return (
+    <div>SparkLine</div>
+  )
+}
+
+export default SparkLine
---
+++ src/pages/charts/Pie.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Pie: React.FC = () => {
+  return <div>Line</div>
+}
+
+export default Pie

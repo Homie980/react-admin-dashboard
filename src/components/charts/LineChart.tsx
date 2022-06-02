abd990a94a95840933d80405f1d72832f19e8d20
Abdelrahman Khaled <abdelrahman@Abdelrahmans-iMac.local>
2022-06-02 19:30:31 +0200

    Line, Area chart done

--- src/components/charts/LineChart.tsx
+++ src/components/charts/LineChart.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React from 'react'
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, LineSeries, DateTime, Legend, Tooltip, ChartRangePadding } from '@syncfusion/ej2-react-charts'
 
-const LineChart: React.FC = () => {
+import { lineCustomSeries, LinePrimaryXAxis, LinePrimaryYAxis } from '../../data/dummy'
+import { useStateContext } from '../../contexts/ContextProvider'
+const Line: React.FC = () => {
+  const { currentMode } = useStateContext()
   return (
-    <div>Line Chart</div>
+    <ChartComponent
+      id='line-chart'
+      height='420px'
+      primaryXAxis={LinePrimaryXAxis as any}
+      primaryYAxis={LinePrimaryYAxis as any}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+    >
+      <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
+      <SeriesCollectionDirective>
+        {lineCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
+      </SeriesCollectionDirective>
+    </ChartComponent>
   )
 }
 
-export default LineChart
+export default Line
--- src/pages/charts/Line.tsx
+++ src/pages/charts/Line.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { LineChart } from '../../components'
+
+import Header from '../../components/Header'
 
 const Line: React.FC = () => {
-  return <div>Line</div>
+  return (
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+      <Header category="Chart" title="Inflation" />
+      <div className='w-full'>
+        <LineChart />
+      </div>
+    </div>
+  )
 }
 
 export default Line
